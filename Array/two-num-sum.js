83242948265d8e0a613783c68d51dca9b9a54c4f
Maya Novarini <mayanovarini@gmail.com>
2020-06-01 23:45:36 -0700

    array of all possible pair of sum

--- Array/two-num-sum.js
+++ Array/two-num-sum.js
@@ -1,5 +1,5 @@
 /* 
-Return one pair of integer from the array that sum up to the target number
+Return one pair of integers from the array that sum up to the target number
 Solution O(nlog(n)) time | O(1) space
 
 Steps:
@@ -18,7 +18,8 @@ const twoNumberSum = (array, targetSum) => {
     while (left < right) {
         let currentSum = array[left] + array[right];
         if (currentSum === targetSum) {
-            return [array[left], array[right]];
+            // loop is done as soon as a pair is found
+            return [array[left], array[right]]; 
         } else if (currentSum < targetSum) { // too small
             left++; // increase the left value
         } else if (currentSum > targetSum) { // too big
@@ -26,4 +27,37 @@ const twoNumberSum = (array, targetSum) => {
         }
     }
     return []; // loop breaks but no pair found
+};
+
+/* Return EVERY pair of integers from the array that sum up to the target number
+Solution O(nlog(n)) time | O(1) space
+
+Steps:
+Sort the array in ascending order
+Use two indexes (leftmost and rightmost) and sum the values
+Loop stops when right and left indexes cross each other
+If the sum is the target sum, returns the pair
+If the sum is too small, increment the left index
+if the sum is too big, decrement the right index
+*/
+
+const allPairSum = (array, targetSum) => {
+    array.sort((a,b) => a - b);
+    let left = 0;
+    let right = array.length - 1;
+    let result = [];
+
+    while (left < right) {
+        let currentSum = array[left] + array[right];
+        if (currentSum === targetSum) {
+            result.push([array[left], array[right]]);
+            left++;
+            right--;
+        } else if (currentSum < targetSum) { // too small
+            left++; // increase the left value
+        } else if (currentSum > targetSum) { // too big
+            right--; // decrease the right value
+        }
+    }
+    return result || []; 
 };
